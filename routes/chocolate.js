49918fd75781044b34f5b25f7fa7d9736a97d2bb
Srinidhi Balmuri <balmurisrinidhi@gmail.com>
2022-11-29 14:58:17 -0600

    bonus

--- routes/chocolate.js
+++ routes/chocolate.js
@@ -17,11 +17,11 @@ router.get('/', chocolate_controlers.chocolate_view_all_Page);
 
 router.get('/detail', chocolate_controlers.chocolate_view_one_Page); 
 
-router.get('/create', chocolate_controlers.chocolate_create_Page); 
+router.get('/create', secured,chocolate_controlers.chocolate_create_Page); 
 
 router.get('/update',secured, chocolate_controlers.chocolate_update_Page);
 
-router.get('/delete', chocolate_controlers.chocolate_delete_Page);
+router.get('/delete',secured, chocolate_controlers.chocolate_delete_Page);
 
 
 
