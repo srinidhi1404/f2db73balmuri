ccdacb3986dd5d4e2100199e96f517f706ccfe6b
Srinidhi Balmuri <balmurisrinidhi@gmail.com>
2022-11-30 21:05:28 -0600

    change

--- models/chocolate.js
+++ models/chocolate.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const chocolateSchema = mongoose.Schema({
     choclateName: {type:String},
     chocolateCost: { type: Number, min: 10, max: 1000 }, 
-    quantityAvailable: {type: Number, min: 0}
+    quantityAvailable: {type: Number, min: 10}
 })
 module.exports = mongoose.model("chocolate",
 chocolateSchema)
